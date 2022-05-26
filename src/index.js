db87d9dec2e10308318d1a9027e290cec04b5731
Tarasish bhattacharya <tarasishbhattacharya06@gmail.com>
2022-05-26 20:16:27 +0530

    a

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Authcontextprovider } from './context/authcontext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+ 
+  <Authcontextprovider>
     <App />
-  </React.StrictMode>
+  </Authcontextprovider>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
